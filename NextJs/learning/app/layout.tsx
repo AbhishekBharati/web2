a12d2a50a8b9fa1d8eaebc55f069451b3620e5f7
AbhishekBharati <210305105048@paruluniversity.ac.in>
2025-01-12 22:38:19 +0530

    Learned Layout

--- NextJs/learning/app/layout.tsx
+++ NextJs/learning/app/layout.tsx
@@ -27,6 +27,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <div className="border-4 p-4"> Quizilio </div>
         {children}
       </body>
     </html>
