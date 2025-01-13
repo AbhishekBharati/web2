d7b338e5ca08d065ba1fe787f5d49793a0397dc7
AbhishekBharati <210305105048@paruluniversity.ac.in>
2025-01-13 20:09:22 +0530

    Learned why we should create another Db file for creating connection from DataBase

--- NextJs/week-19-next-app/app/page.tsx
+++ NextJs/week-19-next-app/app/page.tsx
@@ -1,7 +1,16 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>Hey There</div>
+    <div className="text-lg w-screen h-screen flex items-center justify-center">
+      <div>
+        TodoApplication
+        <br />
+        <Link className="text-md border m-2" href={"/signin"}>Sign in to ToDo App</Link >
+        <br />
+        <Link className="text-md border m-2" href={"/signup"}>Sign up to Todo App</Link>
+      </div>
+    </div>
   );
 }
--- NextJs/week-19-next-app/app/signup/page.tsx
+++ NextJs/week-19-next-app/app/signup/page.tsx
@@ -2,20 +2,23 @@
 
 import axios from "axios";
 import { useState } from "react"
+import { useRouter } from "next/navigation";
 
 export default function Signin() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const router = useRouter();
 
   return <div className="w-screen h-screen flex justify-center items-center">
     <div className="border p-2">
       <input onChange={(e) => setUsername(e.target.value)} type="text" placeholder="username"></input>
       <input onChange={(e) => { setPassword(e.target.value) }} type="text" placeholder="password"></input>
-      <button onClick={() => {
-        axios.post("http://localhost:3000/api/v1/signup", {
+      <button onClick={async () => {
+        await axios.post("http://localhost:3000/api/v1/signup", {
           username,
           password
         });
+        router.push("/signin")
       }} >Signup</button>
     </div>
   </div>
