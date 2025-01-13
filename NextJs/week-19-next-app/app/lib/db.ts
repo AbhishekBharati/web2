9cd531dcce381079399631abd8703790ec371361
AbhishekBharati <210305105048@paruluniversity.ac.in>
2025-01-13 20:50:57 +0530

    Wrote why we need db.ts

--- NextJs/week-19-next-app/app/lib/db.ts
+++ NextJs/week-19-next-app/app/lib/db.ts
@@ -7,3 +7,9 @@ const prisma = globalThis.prisma ?? new PrismaClient();
 if (process.env.NODE_ENV !== 'production') globalThis.prisma = prisma;
 
 export default prisma;
+
+// Alrighty Abhishek why do we need this? --> Coz this will allow us to create prisma client just once
+// Means... There will be only one connection from prisma to database agar mei har page mei use kru
+// toh mei bohat baar client ka connection bna dunga to database and jb bhi hum debug krenge 
+// aur file ko run krenge har baar ek connection create hoga aur eventually humei error aayega ki we have reached
+// maximum limit for the connection
