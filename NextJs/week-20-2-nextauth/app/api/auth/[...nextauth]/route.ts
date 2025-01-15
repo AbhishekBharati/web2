5ff55ec5c5406f94ee7eee3111a2e645d9093a66
AbhishekBharati <210305105048@paruluniversity.ac.in>
2025-01-15 15:17:48 +0530

    Learning nextAuth thoda thoda

--- NextJs/week-20-2-nextauth/app/api/auth/[...nextauth]/route.ts
+++ NextJs/week-20-2-nextauth/app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,33 @@
 import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
 
 const handler = NextAuth({
+  providers: [
+    CredentialsProvider({
+      name: "email",
+      credentials: {
+        username: { label: "Username", type: "text", placeholder: "nodix@gmail.com" },
+        password: { label: "Password", type: "password" }
+      },
+      async authorize(credentials, req) {
+        const username = credentials?.username;
+        const password = credentials?.password;
 
+        //DB requestr to check if this username and password are correct.
+        const user = {
+          name: "Nodix",
+          id: "1",
+          username: "nodix@gmail.com"
+        }
+
+        if (user) {
+          return user;
+        } else {
+          return null;
+        }
+      }
+    })
+  ]
 })
 
 export const GET = handler;
