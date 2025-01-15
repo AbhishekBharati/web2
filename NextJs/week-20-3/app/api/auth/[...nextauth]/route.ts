b007426859c9fe029a5a5c12494b63cf7e023ac6
AbhishekBharati <210305105048@paruluniversity.ac.in>
2025-01-15 20:10:57 +0530

    Learned the base of nextAuth

---
+++ NextJs/week-20-3/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,25 @@
+
+import NextAuth from "next-auth/next";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+const handler = NextAuth({
+  providers: [
+    CredentialsProvider({
+      name: "Email",
+      credentials: {
+        username: { label: "Username", type: "text", placeholder: "jsmith" },
+        password: { label: "Password", type: "password" }
+      },
+      async authorize(credentials, req) {
+        return {
+          username: "Abhishek",
+          id: "1",
+          email: "nodix@gmail.com"
+        }
+      }
+    })
+  ]
+});
+
+export const GET = handler;
+export const POST = handler;
