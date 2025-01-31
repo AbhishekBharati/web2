e250eca8a5a900d69f2af9fe49063b6b2e888c0a
AbhishekBharati <210305105048@paruluniversity.ac.in>
2025-01-31 18:26:20 +0530

    Created a simple inputBox component

---
+++ Monorepos/chat-app/packages/ui/src/text-input.tsx
@@ -0,0 +1,15 @@
+type PropType = {
+  placeholder: string,
+}
+
+export function TextInput({ placeholder }: PropType) {
+  return <input
+    placeholder={placeholder}
+    style={{
+      padding: 10,
+      margin: 10,
+      borderColor: "black",
+      borderWidth: 1
+    }}
+  ></input>
+}
