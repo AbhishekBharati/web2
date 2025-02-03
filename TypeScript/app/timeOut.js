7c13bcc9ffd09f6b95378b4c17145da6d6fa87ad
AbhishekBharati <210305105048@paruluniversity.ac.in>
2025-02-03 14:44:52 +0530

    Learning TypeScript

---
+++ TypeScript/app/timeOut.js
@@ -0,0 +1,8 @@
+"use strict";
+function oneSbaad(fn) {
+    setTimeout(fn, 3000);
+}
+function input() {
+    console.log("helloooo ANshuuu...");
+}
+oneSbaad(input);
---
+++ TypeScript/app/timeOut.ts
@@ -0,0 +1,9 @@
+function oneSbaad(fn: () => void) {
+  setTimeout(fn, 3000);
+}
+
+function input(): void {
+  console.log("helloooo ANshuuu...");
+}
+
+oneSbaad(input);
